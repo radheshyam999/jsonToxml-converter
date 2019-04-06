a659bba3dc153932c239e9e54afd740ef3e90aef
radheshyam999 <radheshalongyamdubey999@gmail.com>
2019-04-06 21:00:36 +0530

    this new project about changing json file to xml file

---
+++ src/Sdk/JsonToXml.java
@@ -0,0 +1,73 @@
+package Sdk;
+
+
+import java.io.*;
+import java.io.File;
+import java.lang.String;
+
+public class JsonToXml{
+	
+	String ReaderPathofile = "E:\\programe\\www\\javascriptproject\\ListOfCountryCode\\js\\code.json";
+	
+    String Writepathfile ="C:\\Users\\ADMIN\\Desktop\\TextFile\\xml\\ls.txt";
+
+    
+void ReadDatafrom_Json_file () throws IOException {
+	//JsonToXml AcessMethod = new JsonToXml();
+	
+		 
+
+		 
+
+		 BufferedReader red = new BufferedReader (new InputStreamReader (System.in));
+
+		 
+	    System.out.println("Pleas write your path for Reading Data:  ");
+	
+	     ReaderPathofile = red.readLine();
+	   ReaderPathofile=  ReaderPathofile.replace ("\\","\\\\");
+	      System.out.println("what  file adress" + ReaderPathofile);
+	      Writepathfile = red.readLine();
+	     Writepathfile=   Writepathfile.replace("\\","\\\\");
+	      File checkfile = new File (ReaderPathofile);
+	      
+	      FileReader readFileFile = new FileReader(ReaderPathofile);
+	      
+	      FileWriter WriteFileInxml = new FileWriter(Writepathfile);
+	
+          BufferedReader ReadFile = new BufferedReader (readFileFile);
+		 
+		  PrintWriter WriteitInfile = new PrintWriter(WriteFileInxml);
+		 
+	     
+	  
+	//if(checkfile.exists ()){
+	 
+			String DataformJsonfile = ReadFile.readLine();
+
+			while (DataformJsonfile != null) {
+				WriteitInfile.println(DataformJsonfile);
+
+				System.out.println(DataformJsonfile);
+
+				DataformJsonfile = ReadFile.readLine();
+
+			}
+	//}
+	//else{
+		//System.out.println("Your Number Is Not Avaliable");
+	//}
+	    
+	
+      WriteFileInxml.flush();
+
+		ReadFile.close();
+		
+	  WriteFileInxml.close();
+	
+	
+	
+}
+
+
+}
---
+++ src/Sdk/Main.java
@@ -0,0 +1,13 @@
+package Sdk;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException
+    {
+        JsonToXml r = new JsonToXml();
+        r.ReadDatafrom_Json_file ();
+    }
+}
+//E:\jsonToXml\src\JsonToXml.java
