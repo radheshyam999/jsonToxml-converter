c891cf6c9ae6f7f51f776ea57d109395d528a118
radheshyam999 <radheshalongyamdubey999@gmail.com>
2019-04-16 17:31:51 +0530

    it working but i have to do more thing s

--- src/Sdk/JsonToXml.java
+++ src/Sdk/JsonToXml.java
@@ -1,6 +1,4 @@
 package Sdk;
-
-
 import java.io.*;
 import java.io.File;
 import java.lang.String;
@@ -9,13 +7,13 @@ public class JsonToXml{
 	
 	String ReaderPathofile = "E:\\programe\\www\\javascriptproject\\ListOfCountryCode\\js\\code.json";
 	
-    String Writepathfile ="C:\\Users\\ADMIN\\Desktop\\TextFile\\xml\\ls.txt";
+   String Writepathfile ="C:\\Users\\ADMIN\\Desktop\\TextFile\\xml\\ls.txt";
 
     
-void ReadDatafrom_Json_file () throws IOException {
+      void ReadDatafrom_Json_file () throws IOException {
 	//JsonToXml AcessMethod = new JsonToXml();
 	
-		 
+		   String rootName,subRootName;
 
 		 
 
@@ -25,29 +23,45 @@ void ReadDatafrom_Json_file () throws IOException {
 	    System.out.println("Pleas write your path for Reading Data:  ");
 	
 	     ReaderPathofile = red.readLine();
-	   ReaderPathofile=  ReaderPathofile.replace ("\\","\\\\");
-	      System.out.println("what  file adress" + ReaderPathofile);
-	      Writepathfile = red.readLine();
-	     Writepathfile=   Writepathfile.replace("\\","\\\\");
-	      File checkfile = new File (ReaderPathofile);
+			
+			 ReaderPathofile=  ReaderPathofile.replace ("\\","\\\\");
+				
+			 System.out.println("what  file adress" + ReaderPathofile);
+				
+				Writepathfile = red.readLine();
+				
+				Writepathfile=   Writepathfile.replace("\\","\\\\");
+				
+				File checkfile = new File (ReaderPathofile);
 	      
 	      FileReader readFileFile = new FileReader(ReaderPathofile);
 	      
 	      FileWriter WriteFileInxml = new FileWriter(Writepathfile);
 	
-          BufferedReader ReadFile = new BufferedReader (readFileFile);
+         BufferedReader ReadFile = new BufferedReader (readFileFile);
 		 
-		  PrintWriter WriteitInfile = new PrintWriter(WriteFileInxml);
-		 
-	     
-	  
+		     PrintWriter WriteitInfile = new PrintWriter(WriteFileInxml);
+	     // ReadFile.replace();
+	      System.out.print ("pleas Enter  your xml file root name ");
+	      rootName = red.readLine ();
+	      System.out .print ("pleas Enter your subroot Name");
+	      subRootName = red.readLine ();
 	//if(checkfile.exists ()){
 	 
 			String DataformJsonfile = ReadFile.readLine();
 
 			while (DataformJsonfile != null) {
 				WriteitInfile.println(DataformJsonfile);
+				DataformJsonfile = DataformJsonfile.replace(":" , "<" + ">");  // here replacing  [ to <>
+			 
+				DataformJsonfile = DataformJsonfile.replace("[", "<" +rootName+ ">");  // here replacing  [ to <>
+				
+  			    DataformJsonfile = DataformJsonfile.replace("]", "</"+rootName + ">"); // here replacing  ] to </>
 
+				DataformJsonfile= DataformJsonfile.replace ("{" ,"<"+subRootName+">");  // here replacing  { to <>
+				
+				DataformJsonfile= DataformJsonfile.replace ("}" ,"</"+subRootName+">");  //// here replacing  } to </>
+				
 				System.out.println(DataformJsonfile);
 
 				DataformJsonfile = ReadFile.readLine();
@@ -61,9 +75,9 @@ void ReadDatafrom_Json_file () throws IOException {
 	
       WriteFileInxml.flush();
 
-		ReadFile.close();
+		  ReadFile.close();
 		
-	  WriteFileInxml.close();
+	     WriteFileInxml.close();
 	
 	
 	
--- src/Sdk/Main.java
+++ src/Sdk/Main.java
@@ -2,12 +2,15 @@ package Sdk;
 
 import java.io.IOException;
 
-public class Main {
+public class Main extends JsonToXml 
+{
 
     public static void main(String[] args) throws IOException
     {
         JsonToXml r = new JsonToXml();
         r.ReadDatafrom_Json_file ();
+        
+    
     }
 }
 //E:\jsonToXml\src\JsonToXml.java
